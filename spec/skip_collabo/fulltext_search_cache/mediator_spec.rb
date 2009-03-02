3dda5bbfe05440e06b4e86ab36477defc9c6b64f
moro <moronatural@gmail.com>
2009-03-02 18:50:01 +0900

    add mediator's test

--- spec/skip_collabo/fulltext_search_cache/mediator_spec.rb
+++ spec/skip_collabo/fulltext_search_cache/mediator_spec.rb
@@ -22,5 +22,28 @@ describe SkipCollabo::FulltextSearchCache, "new('path/to/cache/root', :since => 
       @fts_cache.meta_path(@builder).should == "#{Dir.pwd}/path/to/cache/root/app_cache_meta/file.html"
     end
   end
+
+  describe "#build" do
+
+    before do
+      @model_klass = mock("model_klass")
+      @model_klass.should_receive(:name).and_return "MdelClass"
+      @model_klass.should_receive(:quoted_table_name).and_return "mdoel_classes"
+      @model_klass.should_receive(:scoped).and_return( [model_obj = mock("model_obj")] )
+      model_obj.should_receive(:id).and_return("--id--")
+
+      SkipCollabo::FulltextSearchCache::PartialLoader.should_receive(:new).with([model_obj], 1000).and_return(loader = mock("loader"))
+      loader.should_receive(:each).and_yield( model_obj )
+
+      @builder_klass = mock("builder_klass")
+      @builder_klass.should_receive(:new).with( model_obj ).and_return( builder = mock("builder") )
+      builder.should_receive(:write_cache).with(@fts_cache)
+      builder.should_receive(:write_meta).with(@fts_cache)
+    end
+
+    it do
+      @fts_cache.build(@model_klass, @builder_klass)
+    end
+  end
 end
 
