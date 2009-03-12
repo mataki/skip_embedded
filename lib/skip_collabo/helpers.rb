213cc9a43171a93c46ef178755c888cc09b409c2
moro <moronatural@gmail.com>
2009-03-12 12:32:42 +0900

    ユーザインターフェース部品との連携を行うヘルパを追加

---
+++ lib/skip_collabo/helpers.rb
@@ -0,0 +1,13 @@
+module SkipCollabo
+  module Helpers
+    include Ui
+
+    def self.included(base)
+      ActionView::Helpers::AssetTagHelper.register_javascript_expansion :skip => %w[dropdown_navigation labeled_text_field]
+      ActionView::Helpers::AssetTagHelper.register_javascript_expansion :skip_fckeditor => %w[fckeditor/fckeditor skip_fckeditor_config]
+
+      # TODO skip/style などをどの程度共通化するかを検討、それはではskip_collaboという別名で
+      ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :skip_collabo => %w[skip_collabo]
+    end
+  end
+end
---
+++ lib/skip_collabo/helpers/ui.rb
@@ -0,0 +1,46 @@
+module SkipCollabo
+  module Helpers
+    module Ui
+      # From. http://github.com/mojombo/clippy/tree/master
+      def clippy(copy_text, bgcolor='#FFFFFF')
+        html = <<-EOF
+          <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
+                  width="110"
+                  height="14"
+                  id="clippy" >
+          <param name="movie" value="#{flash_path("clippy")}"/>
+          <param name="allowScriptAccess" value="always" />
+          <param name="quality" value="high" />
+          <param name="scale" value="noscale" />
+          <param NAME="FlashVars" value="text=#{copy_text}">
+          <param name="bgcolor" value="#{bgcolor}">
+          <embed src="#{flash_path("clippy")}"
+                 width="110"
+                 height="14"
+                 name="clippy"
+                 quality="high"
+                 allowScriptAccess="always"
+                 type="application/x-shockwave-flash"
+                 pluginspage="http://www.macromedia.com/go/getflashplayer"
+                 FlashVars="text=#{copy_text}"
+                 bgcolor="#{bgcolor}"
+          />
+          </object>
+        EOF
+      end
+
+      def render_flash(type)
+        if message = flash[type]
+          content_tag("div", :class => type.to_s) do
+            content_tag("h3", h(message) + content_tag("span", _("[Click to hide]")))
+          end
+        end
+      end
+
+      def flash_path(source, suffix="swf")
+        compute_public_path(source, "flash", suffix)
+      end
+    end
+  end
+end
+
