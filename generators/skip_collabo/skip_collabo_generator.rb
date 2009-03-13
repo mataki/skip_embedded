a24473e58fc42642cea788920949e49f536c0d10
moro <moronatural@gmail.com>
2009-03-13 12:28:42 +0900

    insert SkipCollabo::Helper to ApplicationHelper.

--- generators/skip_collabo/skip_collabo_generator.rb
+++ generators/skip_collabo/skip_collabo_generator.rb
@@ -11,6 +11,19 @@ class SkipCollaboGenerator < Rails::Generator::Base
 
       assets.map{|asset| File.dirname(asset) }.uniq.each{|asset| m.directory(asset) }
       assets.each{|asset| m.file asset, asset }
+
+      insert_helper(m) unless options[:pretend]
+    end
+  end
+
+  private
+  def insert_helper(manifest, text = "\n  include SkipCollabo::Helpers", helper = "app/helpers/application_helper.rb")
+    logger.edit_helper(helper)
+    case options[:command]
+    when :create
+      manifest.gsub_file(helper, /^module ApplicationHelper$/){|head| head + text }
+    when :destroy
+      manifest.gsub_file(helper, text, '')
     end
   end
 end
