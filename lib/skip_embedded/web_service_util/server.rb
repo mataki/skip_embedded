9fe5ef1e7a2cd1ed2470b13dceeab0be0b35a05c
moro <moronatural@gmail.com>
2009-03-16 13:55:55 +0900

    refactor

--- lib/skip_embedded/web_service_util/server.rb
+++ lib/skip_embedded/web_service_util/server.rb
@@ -3,7 +3,8 @@ module SkipEmbedded
   class WebServiceUtil
     module Server
       def check_secret_key
-        unless request.env["HTTP_X_SECRET_KEY"] && request.env["HTTP_X_SECRET_KEY"] == SkipEmbedded::InitialSettings['skip_collaboration']['secret_key']
+        req_key = request.env["HTTP_X_SECRET_KEY"] && request.env["HTTP_X_SECRET_KEY"]
+        unless  req_key == InitialSettings['skip_collaboration']['secret_key']
           render :text => { :error => "Forbidden" }.to_json, :status => :forbidden
           return false
         end
