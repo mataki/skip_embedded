03bbdc362e94b1586d5abccd7d532ec47dd654fb
moro <moronatural@gmail.com>
2009-03-19 15:41:47 +0900

    Edit controllers in generator.

--- generators/skip_embedded/skip_embedded_generator.rb
+++ generators/skip_embedded/skip_embedded_generator.rb
@@ -13,18 +13,38 @@ class SkipEmbeddedGenerator < Rails::Generator::Base
       assets.map{|asset| File.dirname(asset) }.uniq.each{|asset| m.directory(asset) }
       assets.each{|asset| m.file asset, asset }
 
-      insert_helper(m) unless options[:pretend]
+      insert_application_helper(m)
+      insert_application_controller(m)
+      insert_sessions_controller(m)
     end
   end
 
   private
-  def insert_helper(manifest, text = "\n  include SkipEmbedded::Helpers", helper = "app/helpers/application_helper.rb")
-    logger.edit_helper(helper)
+  def insert_application_helper(manifest)
+    insert_file( manifest, "app/helpers/application_helper.rb",
+                /^module ApplicationHelper$/,
+                "\n  include SkipEmbedded::Helpers")
+  end
+
+  def insert_application_controller(manifest)
+    insert_file( manifest, "app/controllers/application.rb",
+                /^\s+include Repim::Application\s*$/,
+                "\n  include SkipEmbedded::OpenIdSso::Authentication")
+  end
+
+  def insert_sessions_controller(manifest)
+    insert_file( manifest, "app/controllers/sessions_controller.rb",
+                /^\s+include Repim::RelyingParty\s*$/,
+                "\n  include SkipEmbedded::OpenIdSso::SessionManagement")
+  end
+
+  def insert_file(manifest, file, pattern, text)
+    logger.edit_helper(file)
+    return if options[:pretend]
+
     case options[:command]
-    when :create
-      manifest.gsub_file(helper, /^module ApplicationHelper$/){|head| head + text }
-    when :destroy
-      manifest.gsub_file(helper, text, '')
+    when :create  then manifest.gsub_file(file, pattern){|head| head + text }
+    when :destroy then manifest.gsub_file(file, text, '')
     end
   end
 end
