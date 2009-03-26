706d2392a37ebc59a4786cc53c794167486894d7
moro <moronatural@gmail.com>
2009-03-26 14:45:44 +0900

    add tutorial

--- rails/init.rb
+++ rails/init.rb
@@ -4,3 +4,7 @@ Thread.critical{
   SkipEmbedded::InitialSettings.config = File.expand_path("config/initial_settings.yml", Rails.root)
   SkipEmbedded.instance # force loading
 }
+
+config.gem 'gettext', :lib => 'gettext/rails', :version => '1.93.0'
+config.gem 'moro-repim', :lib => 'repim', :version => '>=0.1.4', :soruce => 'http://gems.github.com/'
+
